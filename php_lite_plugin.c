6bbc0651b0d64f23a83c8a0497b8ff549662585a
Odin Hultgren Van Der Horst <root@edge0.box>
2019-08-12 03:31:40 -0700

    Added filename and num to php log

--- php_lite_plugin.c
+++ php_lite_plugin.c
@@ -368,7 +368,7 @@ static void sapi_uwsgi_log_message(char *message, int syslog_type_int) {
 #else
 static void sapi_uwsgi_log_message(char *message TSRMLS_DC) {
 #endif
-	uwsgi_log(" PHP %s\n", message);
+	_uwsgi_report(zend_get_executed_filename(),zend_get_executed_lineno(),INFO,"PHP %s\n",message);
 }
 int sapi_uwsgi_activate() {
 
