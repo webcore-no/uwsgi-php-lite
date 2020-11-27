09f20a6db9d1ed819ffed6b12b3a253dd4307e9e
Odin Hultgren Van Der Horst <odin@digitalgarden.no>
2020-11-27 14:16:23 +0100

    opcache with php8

--- php_lite_plugin.c
+++ php_lite_plugin.c
@@ -41,7 +41,6 @@ void uwsgi_preload_file(char *name, char *search, char *replace) {
 				zend_file_handle fh;
 				zend_stream_init_fp(&fh, fp, new_name);
 				uwsgi_log("Adding %s to opcache as %s\n",name,new_name);
-
 				if(php_request_startup(TSRMLS_C) == FAILURE) {
 					uwsgi_error("php_request_startup()");
 					return;
@@ -116,11 +115,12 @@ static int sapi_uwsgi_ub_write(const char *str, uint str_length TSRMLS_DC)
 #endif
 {
 	struct wsgi_request *wsgi_req = (struct wsgi_request *) SG(server_context);
-
-	uwsgi_response_write_body_do(wsgi_req, (char *) str, str_length);
-	if (wsgi_req->write_errors > uwsgi.write_errors_tolerance) {
-		php_handle_aborted_connection();
-		return -1;
+	if(wsgi_req) {
+		uwsgi_response_write_body_do(wsgi_req, (char *) str, str_length);
+		if (wsgi_req->write_errors > uwsgi.write_errors_tolerance) {
+			php_handle_aborted_connection();
+			return -1;
+		}
 	}
 	return str_length;
 }
